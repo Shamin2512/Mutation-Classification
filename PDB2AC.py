a092ac4d1368ce2420dec477566fae91925ab4c5
Shamin <shamin.tahasildar@gmail.com>
2023-04-11 17:15:27 +0100

    Prints "Running script" when executed

--- PDB2AC.py
+++ PDB2AC.py
@@ -1,9 +1,4 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[1]:
-
-
+# %%
 """
 This script peforms the pre-processing needed for MutationClassification program. 
 This includes removing NaNs, encoding class label, and converting the PDB codes in a dataset to Uniprot accession codes.
@@ -11,22 +6,18 @@ Only converts the 4 alphanumeric characters, not the chain and residue number.
 
 Returns a processed dataset. 
 """
-
-#!/usr/bin/env python3
-
 from urllib import request
 import sys
 import re
 import pandas as pd
 import numpy as np
 import time
+import xgboost as xgb
+from sklearn.preprocessing import LabelEncoder
 
-
-# In[2]:
-
-
+# %%
 def clean_data(file):
-    df = pd.read_csv('E2.csv')
+    df = pd.read_csv(file)
 
     #Remove unrequired NaNs, blank spaces, reset index to run from 0
     df.dropna(inplace = True)
@@ -34,16 +25,13 @@ def clean_data(file):
     df.reset_index(drop=True, inplace = True)
 
     #Encodes class labels to numeric values (0 or 1)
-    cleaned_encoded = pd.get_dummies(df, columns=['dataset']) #Encode the PD and SNP columns
-    cleaned = cleaned_encoded.drop(['dataset_snp'],axis = 1)
-    
+    df['dataset'] = 1 - (LabelEncoder().fit_transform(df['dataset']))   # Subtract from 1 so that PD = 1 and SNP = 0
+    cleaned = df
+        
     return cleaned
 
-
-# In[3]:
-
-
-def group_data(cleaned):
+# %%
+def group(cleaned):
     group_cleaned = cleaned.sort_values(by=['pdbcode:chain:resnum:mutation'])
 
     PDB_codes = []
@@ -55,10 +43,7 @@ def group_data(cleaned):
 
     return group_cleaned, PDB_codes
 
-
-# In[4]:
-
-
+# %%
 def PDBSWS(PDB_codes, group_cleaned):
     
     AC_codes = []
@@ -76,26 +61,22 @@ def PDBSWS(PDB_codes, group_cleaned):
         
         AC_codes.append(ac)
     group_cleaned.insert(0, 'AC Code', AC_codes)
-    AC_dataset.to_csv('AC_dataset.csv)
+    group_cleaned.to_csv('AC_dataset.csv')
                       
     return AC_codes, group_cleaned
 
-
-# In[5]:
-
-
+# %%
 """ Main program """
+print("Running script...")
 start = time.time()
 
 file = "E2.csv"
 cleaned = clean_data(file)
-group_cleaned, PDB_codes = group_data(cleaned)
+group_cleaned, PDB_codes = group(cleaned)
 AC_codes, AC_dataset = PDBSWS(PDB_codes, group_cleaned)
 end = time.time()
 
-
-# In[6]:
-
-
+# %%
 print(f"Time: {end-start} seconds\n\n{AC_dataset}")
+
 
